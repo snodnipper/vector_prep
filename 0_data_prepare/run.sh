1446561053a234b2daf3271bc26ea18a54005af2
Ollie Snowden <oliversnowden@snodnipper.co.uk>
2017-01-12 18:50:23 +0000

    Update run.sh

--- 0_data_prepare/run.sh
+++ 0_data_prepare/run.sh
@@ -2,19 +2,18 @@
 
 # Build container to fetch data
 echo "Build the data fetcher Container"
-cd 0_data_prepare
+cd 0_data_prepare 
 sudo docker build -t data-download .
 cd ..
 
 echo We are here $(pwd)
-FILENAME="${DOWNLOAD_URL##*/}"
+FILENAME="${DOWNLOAD_MBTILES##*/}"
 echo Filename: "$FILENAME"
 
 # Download if file not already there
 if [ ! -f $FILENAME ]; then
-   echo "Downloading $DOWNLOAD_URL"
-   wget $DOWNLOAD_URL
+   echo "Downloading $DOWNLOAD_MBTILES"
+   wget $DOWNLOAD_MBTILES
 else
    echo "$FILENAME already there...skipping download."
 fi
-
