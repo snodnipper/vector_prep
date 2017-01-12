69a569d890aeebfda67c8aaad9821930bc05b1be
Ollie Snowden <oliversnowden@snodnipper.co.uk>
2017-01-12 19:28:07 +0000

    Update run.sh

--- 0_data_prepare/run.sh
+++ 0_data_prepare/run.sh
@@ -2,7 +2,7 @@
 
 # Build container to fetch data
 echo "Build the data fetcher Container"
-cd 0_data_prepare 
+cd 0_data_prepare
 sudo docker build -t data-download .
 cd ..
 
@@ -13,7 +13,12 @@ echo Filename: "$FILENAME"
 # Download if file not already there
 if [ ! -f $FILENAME ]; then
    echo "Downloading $DOWNLOAD_MBTILES"
-   wget $DOWNLOAD_MBTILES
+   sudo docker run --user root -v $(pwd):/root/ -t data-download /bin/bash -c "wget $DOWNLOAD_MBTILES"
 else
    echo "$FILENAME already there...skipping download."
 fi
+
+
+# Fetch container with mbutil command
+#sudo docker pull jskeates/mbutil
+#sudo docker run -it -v $(pwd):/root/ jskeates/mbutil mbutil --image_format=pbf $FILENAME ./output
