acd3d53d645384a6c79f6d5354bd7310f378b851
Ollie Snowden <oliversnowden@snodnipper.co.uk>
2017-01-12 16:21:37 +0000

    Update run.sh

---
+++ 2_build_sdk_containers/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $EUID != 0 ]; then
+  sudo "$0" "$@"
+  exit $?
+fi
+
+echo Building Mapbox SDK Docker containers
+echo $(pwd)
+cd mapbox-gl-native
+sudo ./docker/build.sh
+sudo ./docker/linux/run.sh
